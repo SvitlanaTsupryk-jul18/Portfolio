fe38c2ab7881763d1ef5fc70c018dff65bc8805d
Svet <stsupryk@gmail.com>
2019-01-30 01:17:41 +0200

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+var browserSync = require("browser-sync").create();
+var autoprefixer = require("gulp-autoprefixer");
+var postcss = require("gulp-postcss");
+var cleanCSS = require("gulp-clean-css");
+//var babel = require("gulp-babel");
+
+var paths = {
+    styles: {
+        src: "sass/**/*.scss",
+        dest: "./css"
+    },
+    scripts: {
+        src: 'scripts/*.js',
+        dest: './'
+    }
+};
+
+function style() {
+    return (gulp
+        .src(paths.styles.src)
+        .pipe(sass())
+        .on("error", sass.logError)
+        .pipe(postcss([autoprefixer(), cleanCSS()]))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(browserSync.stream())
+    );
+}
+
+function scripts() {
+    return gulp
+        .src(paths.scripts.src)
+        .pipe(babel({
+            presets: ['env']
+        }))
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+function reload() {
+    browserSync.reload();
+}
+
+function watch() {
+    browserSync.init({
+        // You can tell browserSync to use this directory and serve it as a mini-server
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(paths.styles.src, style);
+    gulp.watch("path/to/html/*.html", reload);
+    //gulp.watch(paths.scripts.src, scripts);
+}
+//var build = gulp.series(gulp.parallel(style, scripts));
+exports.style = style;
+exports.reload = reload;
+exports.watch = watch;
+exports.default = watch;
