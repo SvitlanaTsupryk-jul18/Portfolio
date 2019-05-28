4b18eeb465488eb39686eacce09987ca3c3b205b
Svet <stsupryk@gmail.com>
2019-05-28 17:00:06 +0300

    minify and uglify  script.js

--- script.js
+++ script.js
@@ -1,95 +1,40 @@
-(function () {
-
-    // invocation
-
-    preloader();
-    smoothScrollLinks();
-    toTop();
-    showTitle();
-    AOS.init();
-
-    //////preloader
-
-    function preloader() {
-        document.body.onload = function () {
-            setTimeout(function () {
-                var preloader = document.querySelector(".preloader");
-                if (!preloader.classList.contains('done')) {
-                    preloader.classList.add('done');
-                }
-            }, 1000)
-        }
-    };
-
-    ////smoothScroll
-
-    function smoothScrollLinks() {
-        let nav = document.querySelector(".navbar-nav");
-        nav.querySelectorAll('a[href^="#"]').forEach(link => {
-            link.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-        });
-        
-        let btnHero = document.querySelector(".hero__toscroll");
-        btnHero.addEventListener('click', function (e) {
-                e.preventDefault();
-                document.querySelector(this.getAttribute('href')).scrollIntoView({
-                    behavior: 'smooth'
-                });
-            });
-    }
-
-    /// button to top
-
-    function toTop() {
-        var buttonTop = document.querySelector("#btn-up");
-        window.onscroll = function () {
-            if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
-                buttonTop.style.opacity = "1";
-            } else {
-                buttonTop.style.opacity = "0";
-            }
-        }
-
-        buttonTop.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.querySelector(".hero").scrollIntoView({
-                behavior: 'smooth'
-            });
-        })
-    }
-
-
-    //show text in example of portfolio
-    function showTitle() {
-
-        let tl = new TimelineMax();
-        tl
-            .from(".navbar-brand", .5, {
-                x: '-300%',
-
-            }, delay = .5)
-            .staggerFrom(".nav-link", .5, {
-                y: '-50',
-                opacity: 0
-            }, 0.1, "-=0.20")
-            .from('#title', 2, {
-                y: '-300%',
-                ease: Bounce.easeOut
+! function () {
+    var e;
+    document.body.onload = function () {
+        setTimeout(function () {
+            var e = document.querySelector(".preloader");
+            e.classList.contains("done") || e.classList.add("done")
+        }, 1e3)
+    }, document.querySelector(".navbar-nav").querySelectorAll('a[href^="#"]').forEach(e => {
+        e.addEventListener("click", function (e) {
+            e.preventDefault(), document.querySelector(this.getAttribute("href")).scrollIntoView({
+                behavior: "smooth"
             })
-            .from('.hero__subtitle', .5, {
-                y: '100',
-                opacity: 0
-            }, "-=1")
-            .from('.hero__toscroll', 1, {
-                backgroundColor: "rgba(52, 58, 64)",
-                opacity: .8,
-                borderRadius: "5px"
-            }, "-=2")
-
-    }
-})();
+        })
+    }), document.querySelector(".hero__toscroll").addEventListener("click", function (e) {
+        e.preventDefault(), document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        })
+    }), e = document.querySelector("#btn-up"), window.onscroll = function () {
+        document.body.scrollTop > 400 || document.documentElement.scrollTop > 400 ? e.style.opacity = "1" : e.style.opacity = "0"
+    }, e.addEventListener("click", function (e) {
+        e.preventDefault(), document.querySelector(".hero").scrollIntoView({
+            behavior: "smooth"
+        })
+    }), (new TimelineMax).from(".navbar-brand", .5, {
+        x: "-300%"
+    }, delay = .5).staggerFrom(".nav-link", .5, {
+        y: "-50",
+        opacity: 0
+    }, .1, "-=0.20").from("#title", 2, {
+        y: "-300%",
+        ease: Bounce.easeOut
+    }).from(".hero__subtitle", .5, {
+        y: "100",
+        opacity: 0
+    }, "-=1").from(".hero__toscroll", 1, {
+        backgroundColor: "rgba(52, 58, 64)",
+        opacity: .8,
+        borderRadius: "5px"
+    }, "-=2"), AOS.init()
+}();
---
+++ script1.js
@@ -0,0 +1,95 @@
+(function () {
+
+    // invocation
+
+    preloader();
+    smoothScrollLinks();
+    toTop();
+    showTitle();
+    AOS.init();
+
+    //////preloader
+
+    function preloader() {
+        document.body.onload = function () {
+            setTimeout(function () {
+                var preloader = document.querySelector(".preloader");
+                if (!preloader.classList.contains('done')) {
+                    preloader.classList.add('done');
+                }
+            }, 1000)
+        }
+    };
+
+    ////smoothScroll
+
+    function smoothScrollLinks() {
+        let nav = document.querySelector(".navbar-nav");
+        nav.querySelectorAll('a[href^="#"]').forEach(link => {
+            link.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+        });
+        
+        let btnHero = document.querySelector(".hero__toscroll");
+        btnHero.addEventListener('click', function (e) {
+                e.preventDefault();
+                document.querySelector(this.getAttribute('href')).scrollIntoView({
+                    behavior: 'smooth'
+                });
+            });
+    }
+
+    /// button to top
+
+    function toTop() {
+        var buttonTop = document.querySelector("#btn-up");
+        window.onscroll = function () {
+            if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+                buttonTop.style.opacity = "1";
+            } else {
+                buttonTop.style.opacity = "0";
+            }
+        }
+
+        buttonTop.addEventListener("click", function (e) {
+            e.preventDefault();
+            document.querySelector(".hero").scrollIntoView({
+                behavior: 'smooth'
+            });
+        })
+    }
+
+
+    //show text in example of portfolio
+    function showTitle() {
+
+        let tl = new TimelineMax();
+        tl
+            .from(".navbar-brand", .5, {
+                x: '-300%',
+
+            }, delay = .5)
+            .staggerFrom(".nav-link", .5, {
+                y: '-50',
+                opacity: 0
+            }, 0.1, "-=0.20")
+            .from('#title', 2, {
+                y: '-300%',
+                ease: Bounce.easeOut
+            })
+            .from('.hero__subtitle', .5, {
+                y: '100',
+                opacity: 0
+            }, "-=1")
+            .from('.hero__toscroll', 1, {
+                backgroundColor: "rgba(52, 58, 64)",
+                opacity: .8,
+                borderRadius: "5px"
+            }, "-=2")
+
+    }
+})();
